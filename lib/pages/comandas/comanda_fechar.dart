7cdbff74497d299376cb1ab738b5a05a6428a920
AkaRed3223 <cesarsales22@gmail.com>
2023-09-06 22:26:43 -0300

    Build 'Fechar Comanda' menu

--- lib/pages/comandas/comanda_fechar.dart
+++ lib/pages/comandas/comanda_fechar.dart
@@ -50,30 +50,51 @@ class _ComandaFecharState extends State<ComandaFechar> {
     return Scaffold(
       backgroundColor: Colors.grey[900],
       appBar: const CustomAppBar(title: 'Fechar Comanda'),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Text(widget.comanda.name,
-                style: TextStyle(
-                    fontSize: MediaQuery.of(context).size.width * 0.06,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold)),
-            Text("${widget.comanda.externalId}",
-                style: TextStyle(
-                    fontSize: MediaQuery.of(context).size.width * 0.18,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold)),
-            Text("Total: ${widget.comanda.totalFormatado}",
-                style: TextStyle(
-                    fontSize: MediaQuery.of(context).size.width * 0.045,
-                    color: Colors.white)),
-            for (Produto produto in widget.comanda.products)
-              Text("${produto.name} - ${produto.precoFormatado}",
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(20.0),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey,
+                  ),
+                  borderRadius: BorderRadius.circular(25.0),
+                  color: Colors.transparent,
+                ),
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Text('Nome do cliente:\n ${widget.comanda.name}',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: MediaQuery.of(context).size.width * 0.06,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold)),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text("${widget.comanda.externalId}",
                   style: TextStyle(
-                      fontSize: MediaQuery.of(context).size.width * 0.045,
-                      color: Colors.white)),
-          ],
+                      fontSize: MediaQuery.of(context).size.width * 0.18,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold)),
+              Text("Total: ${widget.comanda.totalFormatado}",
+                  style: TextStyle(
+                      fontSize: MediaQuery.of(context).size.width * 0.06,
+                      color: Colors.greenAccent,
+                    fontWeight: FontWeight.bold
+                  )),
+              const SizedBox(height: 12,),
+              for (Produto produto in widget.comanda.products)
+                Text("${produto.name} - ${produto.precoFormatado}",
+                    style: TextStyle(
+                        fontSize: MediaQuery.of(context).size.width * 0.045,
+                        color: Colors.white)),
+              const SizedBox(height: 70,),
+            ],
+          ),
         ),
       ),
       bottomSheet: GestureDetector(
